4fd1a8a7c8734f6f6024f23fa9bab197bd41f487
Reo Petersen <Reoapetersen@gmail.com>
2023-04-24 23:17:34 +0000

    Update

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -15,8 +15,7 @@ export default function Home() {
       <div className="container-fluid py-3">
         <h1 className="display-5 fw-bold my-3" id="title">Welcome to the ToDo List!</h1>
         <p className="col-md-12 fs-4 mx-auto my-4" id="homeDisplay">
-          {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni vel aliquid, natus laudantium a beatae adipisci ab quam soluta pariatur, blanditiis totam corrupti sint suscipit! Dolor nam perferendis voluptatibus. */}
-          {res}
+          {res? res : "Ger adipisci felisq praesent nec anunc mauris aenean cras. Dum ulum nascetur uam sque liquam primis tdonec auris. Lla lisis ullam nulla consequa eger porttit mattisae liberout suspendi. Liberos cubilia sociosqu mauris disse leocur ante quam. Mollis rutrum metusqui liberoa facili mi erat. Nec tfusce tasse ellus lobortis turpisut neque nunc mauris. Tortorp quiscras ibulum insuspen esent miproin culus. Mus himena volutpa eratfus tsed estnunc laut fames. Sim que inceptos atein orciduis malesu metusves nunccras dictumst iam. Magna enimphas ullam auctorpr rsed bibendu puruscra. Elemen lus iain eroscu molestie leopelle ecenas fermen. Pretium ulum estmorbi justov nequen loremnul snam. Vehicula ecenas usce setiam ulum sellus nullain miin non."}
         </p>
         <Link to="/todo">
           <button className="btn btn-primary btn-lg float-center mt-5 mb-3" type="button">
